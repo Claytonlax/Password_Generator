35c91382793948affee2707b1c633be8c85aeabe
Claytonlax <130805846+Claytonlax@users.noreply.github.com>
2023-06-13 14:45:02 -0700

    First function and more Variables

--- assets/script.js
+++ assets/script.js
@@ -2,95 +2,37 @@ var generateBtn = document.querySelector("#generate");
 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var specialCharacter = [
-  "!",
-  "#",
-  "$",
-  "%",
-  "&",
-  "'",
-  "(",
-  ")",
-  "*",
-  "^",
-  "+",
-  "-",
-  ".",
-];
+var specialCharacter = [ "!", "#", "$", "%", "&", "'", "(", ")", "*", "^", "+", "-", "."];
 
-var lowercaseLetter = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
+ var lowercaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var uppercaseLetter = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
+var uppercaseLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 var approveLength = "";
 
 var approveSpecialCharacter;
-
 var approveNumberCharacter;
-
 var approveLowercaseLetter;
-
 var approveUppercaseLetter;
 
-var choices1 = [];
-var generatorAlgoIndex = Math.floor(Math.random() * choices1);
-choices1 = generatorAlgoIndex[choices1];
+function generatePassword () {
+  var passwordLength = (prompt("Number of characters to be included in your password?"));
 
-// Get references to the #generate element
+  while (passwordLength <= 7 || passwordLength >= 129) {
+    alert("Input must be between 8-128 characters");
+    var passwordLength = (prompt("Number of characters to be included in your password?"));
+  }
+
+  var approveSpecialCharacter = confirm();
+  var approveNumberCharacter = confirm();
+  var approveLowercaseLetter = confirm();
+  var approveUppercaseLetter = confirm();
+}
+
+// var choices1 = [];
+// var generatorAlgoIndex = Math.floor(Math.random() * choices1);
+// choices1 = generatorAlgoIndex[choices1];
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
