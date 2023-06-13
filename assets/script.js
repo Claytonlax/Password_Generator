f3b12806dccd45085b32e4d35266123927e77977
Claytonlax <130805846+Claytonlax@users.noreply.github.com>
2023-06-13 15:02:26 -0700

    building the function

--- assets/script.js
+++ assets/script.js
@@ -2,34 +2,134 @@ var generateBtn = document.querySelector("#generate");
 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var specialCharacter = [ "!", "#", "$", "%", "&", "'", "(", ")", "*", "^", "+", "-", "."];
+var specialCharacter = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "^",
+  "+",
+  "-",
+  ".",
+];
 
- var lowercaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var lowercaseLetter = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-var uppercaseLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var uppercaseLetter = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-var approveLength = "";
-
-var approveSpecialCharacter;
 var approveNumberCharacter;
 var approveLowercaseLetter;
 var approveUppercaseLetter;
+var approveSpecialCharacter;
+var choices1;
 
-function generatePassword () {
-  var passwordLength = (prompt("Number of characters to be included in your password?"));
+function generatePassword() {
+  var passwordLength = prompt(
+    "Number of characters to be included in your password?"
+  );
 
   while (passwordLength <= 7 || passwordLength >= 129) {
     alert("Input must be between 8-128 characters");
-    var passwordLength = (prompt("Number of characters to be included in your password?"));
+    var passwordLength = prompt(
+      "Number of characters to be included in your password?"
+    );
   }
 
-  var approveSpecialCharacter = confirm();
-  var approveNumberCharacter = confirm();
-  var approveLowercaseLetter = confirm();
-  var approveUppercaseLetter = confirm();
+  var approveNumberCharacter = confirm(
+    "Would you like to include Numeric Characters?"
+  );
+  var approveLowercaseLetter = confirm(
+    "Would you like to include Lowercase Letters?"
+  );
+  var approveUppercaseLetter = confirm(
+    "Would you like to include Uppercase Letters?"
+  );
+  var approveSpecialCharacter = confirm(
+    "Would you like to include Special Charcters?"
+  );
+
+  if (
+    !approveNumberCharacter &&
+    !approveLowercaseLetter &&
+    !approveUppercaseLetter &&
+    !approveSpecialCharacter
+  ) {
+    alert("Atleast one parameter must be chosen");
+    var approveNumberCharacter = confirm(
+      "Would you like to include Numeric Characters?"
+    );
+    var approveLowercaseLetter = confirm(
+      "Would you like to include Lowercase Letters?"
+    );
+    var approveUppercaseLetter = confirm(
+      "Would you like to include Uppercase Letters?"
+    );
+    var approveSpecialCharacter = confirm(
+      "Would you like to include Special Charcters?"
+    );
+  }
 }
 
-// var choices1 = [];
+//
 // var generatorAlgoIndex = Math.floor(Math.random() * choices1);
 // choices1 = generatorAlgoIndex[choices1];
 
@@ -44,5 +144,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-return passwordCriteria;
+document.querySelector("#generate").addEventListener("click", writePassword);
