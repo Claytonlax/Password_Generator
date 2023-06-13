d6adc403fa7819ed1df1fbce3fd3d550c958e8f6
Claytonlax <130805846+Claytonlax@users.noreply.github.com>
2023-06-12 17:27:53 -0700

    more variables

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,94 @@
 var generateBtn = document.querySelector("#generate");
 
-var number = ["0","1","2","3","4","5","6","7","8","9"];
-var specialCharacter = ["!","#","$","%","&","'","(",")","*","^","+","-","."];
-var lowercaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercaseLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+var specialCharacter = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "^",
+  "+",
+  "-",
+  ".",
+];
 
-var passwordCriteria = prompt();
+var lowercaseLetter = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-var choices1 = []
+var uppercaseLetter = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var approveLength = "";
+
+var approveSpecialCharacter;
+
+var approveNumberCharacter;
+
+var approveLowercaseLetter;
+
+var approveUppercaseLetter;
+
+var choices1 = [];
 var generatorAlgoIndex = Math.floor(Math.random() * choices1);
-choices1 = generatorAlgoIndex[choices1]
-
+choices1 = generatorAlgoIndex[choices1];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -22,9 +99,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-return(passwordCriteria)
+return passwordCriteria;
